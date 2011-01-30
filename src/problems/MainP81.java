587a8f6cf5b8257595775cc96c33ae7a3c8204f6
Damien Morcellet <dmorcellet@free.fr>
2011-01-30 15:28:55 +0000

    Euler Project:     * added Problem 82     * shared code for problems 81 and 82

--- src/problems/MainP81.java
+++ src/problems/MainP81.java
@@ -11,20 +11,26 @@ import java.io.InputStreamReader;
  */
 public class MainP81
 {
-  private static final int MATRIX_SIZE=80;
-  private int[][] _matrix;
+  /**
+   * Matrix size for problems 81, 82 and 83.
+   */
+  public static final int MATRIX_SIZE=80;
 
   private MainP81()
   {
     // Nothing to do !
   }
 
-  private void loadMatrix()
+  /**
+   * Matric loader for problems 81, 82 and 83.
+   * @return a matrix.
+   */
+  public static int[][] loadMatrix()
   {
-    _matrix=new int[MATRIX_SIZE][MATRIX_SIZE];
+    int[][] matrix=new int[MATRIX_SIZE][MATRIX_SIZE];
     try
     {
-      InputStream is=getClass().getResourceAsStream("/data/P81-matrix.txt");
+      InputStream is=MainP81.class.getResourceAsStream("/data/P81-matrix.txt");
       BufferedReader r=new BufferedReader(new InputStreamReader(is));
       String line=null;
       for(int lineIndex=0;lineIndex<MATRIX_SIZE;lineIndex++)
@@ -34,7 +40,7 @@ public class MainP81
         String[] items=line.split(",");
         for(int i=0;i<MATRIX_SIZE;i++)
         {
-          _matrix[i][lineIndex]=Integer.parseInt(items[i]);
+          matrix[i][lineIndex]=Integer.parseInt(items[i]);
         }
       }
     }
@@ -42,12 +48,13 @@ public class MainP81
     {
       ioe.printStackTrace();
     }
+    return matrix;
   }
 
   private void doIt()
   {
     long now=System.currentTimeMillis();
-    loadMatrix();
+    int[][] matrix=loadMatrix();
     for(int y=MATRIX_SIZE-1;y>=0;y--)
     {
       for(int x=MATRIX_SIZE-1;x>=0;x--)
@@ -56,23 +63,23 @@ public class MainP81
         {
           if (y<MATRIX_SIZE-1)
           {
-            _matrix[x][y]+=_matrix[x][y+1];
+            matrix[x][y]+=matrix[x][y+1];
           }
         }
         else
         {
           if (y==MATRIX_SIZE-1)
           {
-            _matrix[x][y]+=_matrix[x+1][y];
+            matrix[x][y]+=matrix[x+1][y];
           }
           else
           {
-            _matrix[x][y]+=Math.min(_matrix[x+1][y],_matrix[x][y+1]);
+            matrix[x][y]+=Math.min(matrix[x+1][y],matrix[x][y+1]);
           }
         }
       }
     }
-    int result=_matrix[0][0];
+    int result=matrix[0][0];
     long now2=System.currentTimeMillis();
     System.out.println("result="+result+" ("+(now2-now)+"ms).");
   }
---
+++ src/problems/MainP82.java
@@ -0,0 +1,67 @@
+package problems;
+
+/**
+ * Solved 30.01.2011.
+ * @author DAM
+ */
+public class MainP82
+{
+  private static final int MATRIX_SIZE=MainP81.MATRIX_SIZE;
+
+  private MainP82()
+  {
+    // Nothing to do !
+  }
+
+  private void doIt()
+  {
+    long now=System.currentTimeMillis();
+    int[][] matrix=MainP81.loadMatrix();
+    int[] tmpMin=new int[MATRIX_SIZE];
+    for(int x=1;x<MATRIX_SIZE;x++)
+    {
+      for(int y=0;y<MATRIX_SIZE;y++)
+      {
+        int min=matrix[x-1][y];
+        int sumY=0;
+        for(int y2=y+1;y2<MATRIX_SIZE;y2++)
+        {
+          sumY+=matrix[x][y2];
+          if (sumY>min) break;
+          int sum=matrix[x-1][y2]+sumY;
+          if (sum<min) min=sum;
+        }
+        sumY=0;
+        for(int y2=y-1;y2>=0;y2--)
+        {
+          sumY+=matrix[x][y2];
+          if (sumY>min) break;
+          int sum=matrix[x-1][y2]+sumY;
+          if (sum<min) min=sum;
+        }
+        tmpMin[y]=matrix[x][y]+min;
+      }
+      for(int y=0;y<MATRIX_SIZE;y++) matrix[x][y]=tmpMin[y];
+    }
+    // find min in last column
+    int min=Integer.MAX_VALUE;
+    for(int y=0;y<MATRIX_SIZE;y++)
+    {
+      if (matrix[MATRIX_SIZE-1][y]<min)
+      {
+        min=matrix[MATRIX_SIZE-1][y];
+      }
+    }
+    long now2=System.currentTimeMillis();
+    System.out.println("result="+min+" ("+(now2-now)+"ms).");
+  }
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args)
+  {
+    MainP82 m=new MainP82();
+    m.doIt();
+  }
+}

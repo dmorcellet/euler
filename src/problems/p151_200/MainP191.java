41541999568fbefd4aabe08e0de48498ad2a4de1
Damien Morcellet <dmorcellet@free.fr>
2011-04-03 11:10:36 +0000

    Euler Project:     * memoized version of problem 191 (20 seconds to a few milliseconds)

--- src/problems/p151_200/MainP191.java
+++ src/problems/p151_200/MainP191.java
@@ -8,36 +8,45 @@ public class MainP191
 {
   private static final int MAX_DAYS=30; 
   private static final int MAX_NB_LATE=1; 
-  private static final int MAX_NB_CONSECUTIVE_ABSENT=2; 
+  private static final int MAX_NB_CONSECUTIVE_ABSENT=2;
+
+  // days, nb late, nb absent
+  private long[][][] _memo;
 
   private MainP191()
   {
     // Nothing to do !
   }
 
-  private int evalNextDay(/*char[] data,*/ int index, int nbLate, int nbConsecutiveAbsent)
+  private long evalNextDay(/*char[] data,*/ int daysLeft, int nbLate, int nbConsecutiveAbsent)
   {
-    int ret=0;
-    if (index<MAX_DAYS)
+    long ret=0;
+    if (daysLeft>0)
     {
-      // late
-      if (nbLate<MAX_NB_LATE)
+      ret=_memo[daysLeft][nbLate][nbConsecutiveAbsent];
+      if (ret==-1)
       {
-        //data[index]='L';
-        ret+=evalNextDay(/*data,*/index+1,nbLate+1,0);
+        // late
+        ret=0;
+        if (nbLate<MAX_NB_LATE)
+        {
+          //data[index]='L';
+          ret+=evalNextDay(/*data,*/daysLeft-1,nbLate+1,0);
+          //data[index]=' ';
+        }
+        // absent
+        if (nbConsecutiveAbsent<MAX_NB_CONSECUTIVE_ABSENT)
+        {
+          //data[index]='A';
+          ret+=evalNextDay(/*data,*/daysLeft-1,nbLate,nbConsecutiveAbsent+1);
+          //data[index]=' ';
+        }
+        // present
+        //data[index]='0';
+        ret+=evalNextDay(/*data,*/daysLeft-1,nbLate,0);
         //data[index]=' ';
+        _memo[daysLeft][nbLate][nbConsecutiveAbsent]=ret;
       }
-      // absent
-      if (nbConsecutiveAbsent<MAX_NB_CONSECUTIVE_ABSENT)
-      {
-        //data[index]='A';
-        ret+=evalNextDay(/*data,*/index+1,nbLate,nbConsecutiveAbsent+1);
-        //data[index]=' ';
-      }
-      // present
-      //data[index]='0';
-      ret+=evalNextDay(/*data,*/index+1,nbLate,0);
-      //data[index]=' ';
     }
     else
     {
@@ -51,7 +60,18 @@ public class MainP191
   {
     long now=System.currentTimeMillis();
     //char[] data=new char[MAX_DAYS];
-    int nb=evalNextDay(/*data,*/0,0,0);
+    // init memo
+    _memo=new long[MAX_DAYS+1][][];
+    for(int i=0;i<MAX_DAYS+1;i++)
+    {
+      _memo[i]=new long[MAX_NB_LATE+1][];
+      for(int j=0;j<MAX_NB_LATE+1;j++)
+      {
+        _memo[i][j]=new long[MAX_NB_CONSECUTIVE_ABSENT+1];
+        for(int k=0;k<MAX_NB_CONSECUTIVE_ABSENT+1;k++) _memo[i][j][k]=-1;
+      }
+    }
+    long nb=evalNextDay(/*data,*/MAX_DAYS,0,0);
     long now2=System.currentTimeMillis();
     System.out.println("result="+nb+" ("+(now2-now)+"ms).");
   }

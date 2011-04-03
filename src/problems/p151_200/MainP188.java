a1d91d25efe4a974bd33d4ff11f282ec9d786494
Damien Morcellet <dmorcellet@free.fr>
2011-04-03 13:23:21 +0000

    Euler Project:     * added Problem 188

---
+++ src/problems/p151_200/MainP188.java
@@ -0,0 +1,54 @@
+package problems.p151_200;
+
+import problems.primes.ErathostenesSieve;
+
+/**
+ * Solved 03.04.2011.
+ * @author DAM
+ */
+public class MainP188
+{
+  private static final int MAX=100000000;
+  private static final int SQRT_MAX=(int)Math.sqrt(MAX)+1;
+
+  private MainP188()
+  {
+    // Nothing to do !
+  }
+
+  private void doIt()
+  {
+    long now=System.currentTimeMillis();
+    ErathostenesSieve s=new ErathostenesSieve();
+    long[] primes=s.fill(MAX/2);
+    int nbPrimes=primes.length;
+    int nb=0;
+    int index=0;
+    while(true)
+    {
+      if (index>=nbPrimes) break;
+      long p=primes[index];
+      if (p<SQRT_MAX)
+      {
+        for(int i=index;i<nbPrimes;i++)
+        {
+          if (p*primes[i]>=MAX) break;
+          nb++;
+        }
+      }
+      else break;
+      index++;
+    }
+    long now2=System.currentTimeMillis();
+    System.out.println("result="+nb+" ("+(now2-now)+"ms).");
+  }
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args)
+  {
+    MainP188 m=new MainP188();
+    m.doIt();
+  }
+}
